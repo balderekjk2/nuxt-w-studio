c452755cccf18fcf8e0aa70df78ef349f68b38d3
Derek Bal <bal.derek.j@gmail.com>
2025-08-02 15:23:14 -1000

    nuxt-security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/content',
-    '@nuxt/image',
-    '@nuxt/ui',
-    // 'nuxt-security'
-  ],
+  modules: ['@nuxt/content', '@nuxt/image', '@nuxt/ui', 'nuxt-security'],
   css: ['~/assets/css/main.css'],
-  // security: {
-  //   headers: {
-  //     contentSecurityPolicy: {
-  //       'base-uri': ["'none'"],
-  //       'upgrade-insecure-requests': false,
-  //     },
-  //   },
-  // },
+  security: {
+    headers: {
+      contentSecurityPolicy: {
+        'base-uri': ["'none'"],
+        'upgrade-insecure-requests': false,
+      },
+    },
+  },
 });
